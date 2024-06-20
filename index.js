07cce0be323114fa7137d5bbe4cdfc11f98282e0
FilipSzkarlat <filip.szkarlat@gmail.com>
2024-06-20 16:14:37 +0200

    customizing styles to the app and adding enter as a button click

--- index.js
+++ index.js
@@ -72,3 +72,14 @@ function appendItemToShoppingListEl(item) {
 
   shoppingListEl.append(newEl);
 }
+
+// Execute a function when the user presses a key on the keyboard
+inputFieldEl.addEventListener("keypress", function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    addButtonEl.click();
+  }
+});
